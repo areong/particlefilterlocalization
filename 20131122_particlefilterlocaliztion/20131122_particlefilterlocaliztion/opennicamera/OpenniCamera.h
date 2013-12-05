60ebbcd013b9beaca704e718fb459ae38341fb89
areong <areong6378@gmail.com>
2013-12-05 10:03:38 +0800

    Declare methods of OpenniCamera.

--- 20131122_particlefilterlocaliztion/20131122_particlefilterlocaliztion/opennicamera/OpenniCamera.h
+++ 20131122_particlefilterlocaliztion/20131122_particlefilterlocaliztion/opennicamera/OpenniCamera.h
@@ -1,8 +1,29 @@
-#pragma once
-class OpenniCamera
-{
+#ifndef OPENNICAMERA_H
+#define OPENNICAMERA_H
+
+#include <iostream>
+#include "OpenNI.h"
+using namespace std;
+using namespace openni;
+
+class OpenniCamera {
 public:
     OpenniCamera(void);
     ~OpenniCamera(void);
+    int initialize();
+    void setSamplingMethod();
+    void takeNewDepthPhoto(double xFront, double yFront, double zFront, 
+                           double xTop, double yTop, double zTop);
+    int *getDepthPhoto();
+    int *getDepthPhotoSampled();
+    double *getSamplingVectorsCameraCoordinates();
+    double *getSamplingVectorsRealWorldCoordinates();
+
+private:
+    DepthPixel* depthPhoto;
+    int *depthPhotoSampled;
+    double *samplingVectorsCameraCoordinates;
+    double *samplingVectorsRealWorldCoordinates;
 };
 
+#endif
